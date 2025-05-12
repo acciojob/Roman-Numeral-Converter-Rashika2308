d228bdbb7acf7924cb5970c56b65610fe0792a6c
Rashika2308 <rashikavidyarthi545@gmail.com>
2025-05-12 10:13:25 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,27 @@
+function toRoman(num) {
+  if (num <= 0 || num > 100000) {
+    return "Invalid input";
+  }
+
+  const romanMap = [
+    ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400],
+    ['C', 100], ['XC', 90], ['L', 50], ['XL', 40],
+    ['X', 10], ['IX', 9], ['V', 5], ['IV', 4],
+    ['I', 1]
+  ];
+
+  let result = "";
+
+  for (let [roman, value] of romanMap) {
+    while (num >= value) {
+      result += roman;
+      num -= value;
+    }
+  }
+
+  return result;
+}
+
+// Example usage:
+const input = prompt("Enter a string");   // Output: XIV
+console.log(toRoman(input));   // Output: DCCXCVIII
